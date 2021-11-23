4c01398f0ba1d5192f2be74d77adbd969ba33da4
poma <semenov.roma@gmail.com>
2021-11-23 07:09:01 +0300

    add usage msg

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const path = require('path')
 const { utils } = require('ethers')
 const trim = str => str.replace(/\.0$/, '')
 const [, unit, number] = process.argv
+if (!number) {
+  console.log(`Usage: ${path.basename(unit)} <number>`)
+  process.exit(1)
+}
 const value = utils.parseUnits(number, path.basename(unit))
 console.log(`wei ${trim(utils.formatUnits(value, 'wei'))}`)
 console.log(`gwei ${trim(utils.formatUnits(value, 'gwei'))}`)
